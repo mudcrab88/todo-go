51b32128d5815782593d88e62dce0ec5ef77fdf7
николай кузнецов <ni.kuznetsov@bars.group>
2023-09-09 23:20:50 +0300

    Add services and repo

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/mudcrab88/todo"
 	"github.com/mudcrab88/todo/pkg/handler"
+	"github.com/mudcrab88/todo/pkg/repository"
+	"github.com/mudcrab88/todo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
