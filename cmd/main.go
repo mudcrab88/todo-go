3fb6843a8637d90c6eb55e74c4e39b26543d4ea8
николай кузнецов <ni.kuznetsov@bars.group>
2023-09-10 22:44:52 +0300

    Add migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mudcrab88/todo/pkg/handler"
 	"github.com/mudcrab88/todo/pkg/repository"
 	"github.com/mudcrab88/todo/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -19,4 +20,11 @@ func main() {
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
